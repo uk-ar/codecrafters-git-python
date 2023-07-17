41021acb8950498e7e02f004895bab3ff10f0ad7
Yuuki Arisawa <yuuki.ari@gmail.com>
2023-07-17 12:32:47 +0000

    Support cat-file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import sys
 import os
-
+import zlib
+import re
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     #
@@ -16,6 +17,15 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        sha1 = sys.argv[3]
+        with open(".git/objects/"+sha1[:2]+"/"+sha1[2:],"rb") as f:
+            s = f.read()
+        kind,rest = zlib.decompress(s).split(b" ",maxsplit=1)
+        size,rest = rest.split(b"\0",maxsplit=1)
+        #print(kind,size,rest)
+        print(rest.decode(),end="")
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
