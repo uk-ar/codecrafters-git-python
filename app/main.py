56c6ba48f3233ad1723400e1f495482a3113f332
Yuuki Arisawa <yuuki.ari@gmail.com>
2023-07-17 20:14:54 +0000

    Support hash-object

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import zlib
-import re
+import hashlib
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,13 +20,23 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         sha1 = sys.argv[3]
-        with open(".git/objects/"+sha1[:2]+"/"+sha1[2:],"rb") as f:
+        with open(".git/objects/"+sha1[:2]+"/"+sha1[2:], "rb") as f:
             s = f.read()
-        kind,rest = zlib.decompress(s).split(b" ",maxsplit=1)
-        size,rest = rest.split(b"\0",maxsplit=1)
-        #print(kind,size,rest)
-        print(rest.decode(),end="")
-
+        # print(hashlib.sha1(zlib.decompress(s)).hexdigest())
+        kind, rest = zlib.decompress(s).split(b" ", maxsplit=1)
+        size, rest = rest.split(b"\0", maxsplit=1)
+        # print(kind,size,rest)
+        print(rest.decode(), end="")
+    elif command == "hash-object":
+        file = sys.argv[3]
+        with open(file, "rb") as f:
+            s = f.read()
+        s = b"blob " + str(len(s)).encode()+b'\0'+s
+        sha1 = hashlib.sha1(s).hexdigest()
+        os.makedirs(".git/objects/"+sha1[:2],exist_ok=True)
+        with open(".git/objects/"+sha1[:2]+"/"+sha1[2:], "wb") as f:
+            f.write(zlib.compress(s))
+        print(sha1)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
